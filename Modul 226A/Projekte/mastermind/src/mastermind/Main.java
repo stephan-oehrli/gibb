8a7457e7574b4f25b9562c669fea2502d3b9d8d7
Stephan Oehrli <soe116920@iet-gibb.ch>
2019-08-22 08:55:44 +0200

    Kompetenz 3.2.2 bearbeitet

--- Modul 226A/Projekte/mastermind/src/mastermind/Main.java
+++ Modul 226A/Projekte/mastermind/src/mastermind/Main.java
@@ -17,6 +17,8 @@ public class Main {
 		System.out.println("Geben Sie etwas ein:");
 		String userInput = getUserInput();
 		System.out.println(userInput);
+		String[] a = "abcd".split("");
+		System.out.println(a[1]);
 	}
 
 	public static String getUserInput() {
