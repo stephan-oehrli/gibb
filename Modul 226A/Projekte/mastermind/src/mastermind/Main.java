20b3184fcfd838afc81248a87ef323d04eb3ee17
Stephan Oehrli <soe116920@iet-gibb.ch>
2019-08-26 22:26:54 +0200

    Das Programm kann nun einen Geheimcode generieren.

--- Modul 226A/Projekte/mastermind/src/mastermind/Main.java
+++ Modul 226A/Projekte/mastermind/src/mastermind/Main.java
@@ -3,25 +3,23 @@ package mastermind;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
-	public final static String INSTRUCTION = "Geben Sie einen Versuchscode mit vier Buchstaben aus der Menge {r,g,b,y,s,w} ein";
-	public final static String GAME_END = "Spiel beendet. Geheimcode war xxxx. Anzahl Versuche: 5";
-
-	private static InputStreamReader inputReader = new InputStreamReader(System.in);
-	private static BufferedReader reader = new BufferedReader(inputReader);
-
+	
+	private static String[] secretCode;
+	private static int numberOfAttempts;
+	
 	public static void main(String[] args) {
-		System.out.println(INSTRUCTION);
-		System.out.println(GAME_END);
-		System.out.println("Geben Sie etwas ein:");
-		String userInput = getUserInput();
-		System.out.println(userInput);
-		String[] a = "abcd".split("");
-		System.out.println(a[1]);
+		secretCode = generateSecretCode();
+		displayInstructions();
+		displayGameOverMessage();
 	}
 
-	public static String getUserInput() {
+	private static String readUserInput() {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String userInput = "";
 		try {
 			userInput = reader.readLine();
@@ -31,4 +29,22 @@ public class Main {
 		return userInput;
 	}
 
+	private static String[] generateSecretCode() {
+		Random random = new Random();
+		ArrayList<String> colors = new ArrayList<>(Arrays.asList("r", "g", "b", "y", "s", "w"));
+		int codeLength = 4;
+		String[] secretCode = new String[codeLength];
+		for (int i = 0; i < codeLength; i++) {
+			secretCode[i] = colors.remove(random.nextInt(colors.size()));
+		}
+		return secretCode;
+	}
+	
+	private static void displayInstructions() {
+		System.out.println("Geben Sie einen Versuchscode mit vier Buchstaben aus der Menge {r,g,b,y,s,w} ein");
+	}
+
+	private static void displayGameOverMessage() {
+		System.out.println("Spiel beendet. Geheimcode war " + String.join(",", secretCode) + ". Anzahl Versuche: " + numberOfAttempts);
+	}
 }
