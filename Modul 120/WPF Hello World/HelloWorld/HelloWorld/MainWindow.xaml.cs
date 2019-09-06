243496e9c89f4ca96cc25c50724985bac7b8ca80
Stephan Oehrli <soe116920@iet-gibb.ch>
2019-09-06 09:46:29 +0200

    Kompetenz B3: UserControl hinzugefügt

--- Modul 120/WPF Hello World/HelloWorld/HelloWorld/MainWindow.xaml.cs
+++ Modul 120/WPF Hello World/HelloWorld/HelloWorld/MainWindow.xaml.cs
@@ -1,20 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-
-using System.Linq;
-
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HelloWorld
 {
@@ -27,9 +12,11 @@ namespace HelloWorld
         public MainWindow()
         {
             InitializeComponent();
-            
-            
-            
+        }
+
+        private void Label_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ((Label)sender).Content = "Hihihihi!";
         }
     }
 }
