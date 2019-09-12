d10aa0cd3f0d8613be53e2ffe6f7d1a844a7eda8
Stephan Oehrli <soe116920@iet-gibb.ch>
2019-09-12 13:12:11 +0200

    Kompetenz B3: UserControl OnOffButton erstellt

---
+++ Modul 120/AB120_B3_OnOff/AB120_B3_OnOff/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AB120_B3_OnOff
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Modul 120/AB120_B3_OnOff/AB120_B3_OnOff/OnOffButton.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AB120_B3_OnOff
+{
+    /// <summary>
+    /// Interaction logic for OnOffButton.xaml
+    /// </summary>
+    public partial class OnOffButton : UserControl
+    {
+        private bool _zustand = true;
+        bool Zustand
+        {
+            get { return _zustand; }
+            set
+            {
+                _zustand = value;
+                if (value)
+                {
+                    Thickness abstandKreis = Kreis.Margin;
+                    abstandKreis.Left = 5;
+                    Kreis.Margin = abstandKreis;
+                    Kreis.Fill = (Brush)(new BrushConverter().ConvertFrom("#3393FF"));
+                    Kreis.Stroke = (Brush)(new BrushConverter().ConvertFrom("#3393FF"));
+                    Umrahmung.Stroke = (Brush)(new BrushConverter().ConvertFrom("#3393FF"));
+                    Beschriftung.Content = "ON";
+                    Beschriftung.FontWeight = FontWeights.Bold;
+                    Beschriftung.Foreground = (Brush)(new BrushConverter().ConvertFrom("#3393FF"));
+                    Thickness abstandBeschriftung = Beschriftung.Margin;
+                    abstandBeschriftung.Left = 30;
+                    Beschriftung.Margin = abstandBeschriftung;
+                } else
+                {
+                    Thickness abstandKreis = Kreis.Margin;
+                    abstandKreis.Left = 40;
+                    Kreis.Margin = abstandKreis;
+                    Kreis.Fill = Brushes.Black;
+                    Kreis.Stroke = Brushes.Black;
+                    Umrahmung.Stroke = Brushes.Black;
+                    Beschriftung.Content = "OFF";
+                    Beschriftung.FontWeight = FontWeights.Normal;
+                    Beschriftung.Foreground = Brushes.Black;
+                    Thickness abstandBeschriftung = Beschriftung.Margin;
+                    abstandBeschriftung.Left = 7;
+                    Beschriftung.Margin = abstandBeschriftung;
+                }
+            }
+        }
+
+        public OnOffButton()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Zustand = !Zustand;
+        }
+    }
+}
