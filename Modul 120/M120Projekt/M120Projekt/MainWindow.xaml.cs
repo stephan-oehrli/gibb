d41566ba204a0fcfd36d34d750c7e7ca0539a812
Stephan Oehrli <soe116920@iet-gibb.ch>
2019-09-15 20:33:59 +0200

    New Files after program build

--- Modul 120/M120Projekt/M120Projekt/MainWindow.xaml.cs
+++ Modul 120/M120Projekt/M120Projekt/MainWindow.xaml.cs
@@ -24,11 +24,24 @@ namespace M120Projekt
         public MainWindow()
         {
             InitializeComponent();
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
             // Wichtig!
             Data.Global.context = new Data.Context();
             // Aufruf diverse APIDemo Methoden
             APIDemo.DemoACreate();
             APIDemo.DemoACreateKurz();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
         }
     }
 }
