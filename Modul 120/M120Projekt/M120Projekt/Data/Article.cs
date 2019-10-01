145d94f0b987cb2a986a0175aeb0fe50d297e040
Stephan Oehrli <stephan.oehrli@gmx.ch>
2019-10-01 23:09:43 +0200

    Refactoring: Ungenutzte Using-Direktiven entfernt

--- Modul 120/M120Projekt/M120Projekt/Data/Article.cs
+++ Modul 120/M120Projekt/M120Projekt/Data/Article.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity;
 
 namespace M120Projekt.Data
 {
--- Modul 120/M120Projekt/M120Projekt/Migrations/Configuration.cs
+++ Modul 120/M120Projekt/M120Projekt/Migrations/Configuration.cs
@@ -1,9 +1,6 @@
 namespace M120Projekt.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<M120Projekt.Data.Context>
     {
