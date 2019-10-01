be99d368a337b68f7376cd36428409bf21b7fdda
Stephan Oehrli <stephan.oehrli@gmx.ch>
2019-10-01 23:05:01 +0200

    Modul 120, K: C3 - Formularvalidierung hinzugefügt

--- Modul 120/M120Projekt/M120Projekt/UserControls/IndicateArticle.xaml.cs
+++ Modul 120/M120Projekt/M120Projekt/UserControls/IndicateArticle.xaml.cs
@@ -1,24 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using M120Projekt.Data;
 
 namespace M120Projekt.UserControls
 {
-    /// <summary>
-    /// Interaction logic for IndicateArticle.xaml
-    /// </summary>
     public partial class IndicateArticle : UserControl
     {
         public IndicateArticle()
@@ -49,6 +35,5 @@ namespace M120Projekt.UserControls
             article.LastArrival = DateTime.Parse("01/01/2019");
             return article;
         }
-
     }
 }
