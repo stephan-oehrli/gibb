bb4c11f1155eb7d0b4c4cb74619e116172d4ecf1
Stephan Oehrli <soe116920@iet-gibb.ch>
2019-10-17 08:34:44 +0200

    serialVersionUID hinzugefügt

--- Modul 226A/Projekte/graphicalOutputJFrame/src/graphicalOutputJFrame/swingCreatedWindow.java
+++ Modul 226A/Projekte/graphicalOutputJFrame/src/graphicalOutputJFrame/swingCreatedWindow.java
@@ -9,21 +9,22 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class swingCreatedWindow extends JFrame{
+public class swingCreatedWindow extends JFrame {
+	private static final long serialVersionUID = 1L;
 
 	public swingCreatedWindow() throws HeadlessException {
 		super("Mein Beispiel mit grafischer Ausgabe");
-		
+
 		Icon icon = new ImageIcon(getClass().getResource("gibbix.gif"));
 		JLabel label = new JLabel("Java-Programme in der gibbix-Umgebung", icon, JLabel.CENTER);
 		add(label);
-		
+
 		Font font = new Font("SansSerif", Font.BOLD, 20);
 		label.setFont(font);
 		label.setForeground(Color.blue);
 		label.setBackground(Color.lightGray);
 		label.setOpaque(true);
-		
+
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(750, 200);
 		setVisible(true);
@@ -32,5 +33,5 @@ public class swingCreatedWindow extends JFrame{
 	public static void main(String[] args) {
 		new swingCreatedWindow();
 	}
-	
+
 }
