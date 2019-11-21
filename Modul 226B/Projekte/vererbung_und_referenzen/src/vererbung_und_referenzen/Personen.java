85b13b5b994fff2d8fe6e86a818d4985058efb2d
Stephan Oehrli <soe116920@iet-gibb.ch>
2019-11-21 08:47:03 +0100

    Kompetenz 3.2 - Überschreiben von Methoden

--- Modul 226B/Projekte/vererbung_und_referenzen/src/vererbung_und_referenzen/Personen.java
+++ Modul 226B/Projekte/vererbung_und_referenzen/src/vererbung_und_referenzen/Personen.java
@@ -8,7 +8,7 @@ class Firma {
 	}
 }
 
-class Person {
+abstract class Person {
 	protected String name;
 	protected String vorname;
 	protected int personalNummer;
@@ -18,6 +18,15 @@ class Person {
 		this.vorname = vorname;
 		this.personalNummer = personalNummer;
 	}
+	
+	public void print() {
+		System.out.println("---------------------------------");
+		System.out.println("Personalnummer: " + personalNummer);
+		System.out.println("Name: " + name);
+		System.out.println("Vorname: " + vorname);
+	}
+	
+	public abstract int berechneFerien(int alter);
 }
 
 class Chef extends Person {
@@ -26,6 +35,20 @@ class Chef extends Person {
 	public Chef(String name, String vorname, int personalNummer, String abteilung) {
 		super(name, vorname, personalNummer);
 		this.abteilung = abteilung;
+	}
+
+	@Override
+	public void print() {
+		super.print();
+		System.out.println("Abteilung: " + abteilung);
+	}
+
+	@Override
+	public int berechneFerien(int alter) {
+		if (alter > 55) {
+			return 6;
+		}
+		return (alter >= 45) ? 5 : 4;
 	}
 }
 
@@ -36,14 +59,48 @@ class Fachangestellter extends Person {
 		super(name, vorname, personalNummer);
 		this.vorgesetzter = vorgesetzer;
 	}
+
+	@Override
+	public int berechneFerien(int alter) {
+		if (alter > 60) {
+			return 6;
+		}
+		return (alter >= 50) ? 5 : 4;
+	}
+}
+
+class Lernender extends Person {
+	int lehrjahr;
+	
+	public Lernender(String name, String vorname, int personalNummer, int lehrjahr) {
+		super(name, vorname, personalNummer);
+		this.lehrjahr = lehrjahr;
+	}
+
+	@Override
+	public void print() {
+		super.print();
+		System.out.println("Lehrjahr: " + lehrjahr + ". Lehrjahr");
+	}
+
+	@Override
+	public int berechneFerien(int alter) {
+		return 5;
+	}
 }
 
 public class Personen {
 	public static void main(String[] args) {
-		Firma f = new Firma(new Person[] { new Chef("Sattler", "Beatrice", 25, "Verkauf") });
+//		Firma f = new Firma(new Person[] { new Chef("Sattler", "Beatrice", 25, "Verkauf") });
 		
-		Person person = new Person("test", "test", 1);
-		Chef chef = new Chef("Hans", "Müller", 2, "Verkauf");
+		Chef chef = new Chef("Hans", "Müller", 1, "Verkauf");
+		Fachangestellter fachangestellter = new Fachangestellter("Anna", "Frey", 2, chef);
+		Lernender lernender = new Lernender("Julian", "Lüthi", 3, 2);
 		
+		Person[] angestellte = new Person[] {chef, fachangestellter, lernender};
+		for (Person person : angestellte) {
+			person.print();
+			System.out.println("Der Ferienanspruch beträgt: " + person.berechneFerien(46) + " Wochen");
+		}
 	}
 }
