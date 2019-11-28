3c85c6fd34c2c99845845a0405114c6484ead263
Stephan Oehrli <soe116920@iet-gibb.ch>
2019-11-28 09:09:18 +0100

    Delegation der Methoden

--- Modul 226B/Projekte/polymorphismus_und_delegation/src/polymorphismus_und_delegation/Bibliothek.java
+++ Modul 226B/Projekte/polymorphismus_und_delegation/src/polymorphismus_und_delegation/Bibliothek.java
@@ -5,40 +5,47 @@ public class Bibliothek {
 	private Medium[] katalog;
 
 	public void showSample(Medium m) {
-		if (m instanceof Buch) {
-			System.out.println(((Buch) m).sample);
-		} else if (m instanceof CD) {
-			Player p = new Player();
-			p.playSound(((CD) m).sample);
-		} else if (m instanceof DVD) {
-			Player p = new Player();
-			p.playVideo(((DVD) m).sample);
-		}
+		m.showSample();
 	}
-
 }
 
-abstract class Medium {}
+abstract class Medium {
+	public abstract void showSample();
+}
 
 class Buch extends Medium {
 	String sample;
+
+	@Override
+	public void showSample() {
+		System.out.println(sample);
+	}
 }
 
 class CD extends Medium {
 	Sound sample;
+
+	@Override
+	public void showSample() {
+		Player p = new Player();
+		p.playSound(sample);
+	}
 }
 
 class DVD extends Medium {
 	Video sample;
+
+	@Override
+	public void showSample() {
+		Player p = new Player();
+		p.playVideo(sample);
+	}
 }
 
 class Sound {}
 class Video {}
 
 class Player {
-	public void playVideo(Video sample) {
-	}
-	
-	public void playSound(Sound sample) {
-	}
+	public void playVideo(Video sample) {}
+	public void playSound(Sound sample) {}
 }
