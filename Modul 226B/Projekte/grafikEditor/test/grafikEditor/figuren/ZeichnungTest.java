ef07640a525f76c59be9a3e0a120d738111b72e3
Oehrli, Stephan <stephan.oehrli@adesso.ch>
2020-01-17 13:41:20 +0100

    Erstelle ZeichnungTest Klasse

---
+++ Modul 226B/Projekte/grafikEditor/test/grafikEditor/figuren/ZeichnungTest.java
@@ -0,0 +1,14 @@
+package grafikEditor.figuren;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ZeichnungTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
