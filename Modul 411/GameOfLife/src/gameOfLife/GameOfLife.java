3dc50b1b8ff2b03498764929024d4ffebe688a1c
Stephan Oehrli <stephan.oehrli@gmx.ch>
2020-02-07 09:54:21 +0100

    Initialisiere lebendige Zellen mit Prozentangabe

--- Modul 411/GameOfLife/src/gameOfLife/GameOfLife.java
+++ Modul 411/GameOfLife/src/gameOfLife/GameOfLife.java
@@ -7,17 +7,55 @@ public class GameOfLife {
 	private char[][][] world = new char[2][25][25];
 
 	
-	public GameOfLife(int numberOfCellsAtStart) throws InterruptedException {
+	public GameOfLife(int livingCellsInPercentage) throws InterruptedException {
 		createWorld();
-		addRandomCells(numberOfCellsAtStart);
-		
+		addRandomCells(livingCellsInPercentage);
 		for (int i = 0; i < 100; i++) {
 			clearScreen();
 			System.out.println();
 			Thread.sleep(50);
-			printWorld(0);
+			printWorld();
 			calculateGeneration();
 			Thread.sleep(150);
+		}
+	}
+
+	private void createWorld() {
+		for (int i = 0; i < world[0].length; i++) {
+			for (int j = 0; j < world[0][0].length; j++) {
+				world[0][i][j] = '.';
+				world[1][i][j] = '.';
+			}
+		}
+	}
+
+	private void addRandomCells(int livingCellsInPercentage) {
+		Random random = new Random();
+		int livingCells = world[0][0].length * world[0][0].length / 100 * livingCellsInPercentage; 
+		int x, y;
+		for (int i = 0; i < livingCells; i++) {
+			do {
+			y = random.nextInt(world[0].length);
+			x = random.nextInt(world[0].length);
+			} while (world[0][y][x] == '*');
+			world[0][y][x] = '*';
+		}
+	}
+
+	private void clearScreen() {  
+	    try {
+			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void printWorld() {
+		for (int i = 0; i < world[0].length; i++) {
+			for (int j = 0; j < world[0][0].length; j++) {
+				System.out.print(world[0][i][j]);
+			}
+			System.out.println();
 		}
 	}
 
@@ -29,14 +67,6 @@ public class GameOfLife {
 			}
 		}
 		copy();
-	}
-
-	private void copy() {
-		for (int i = 0; i < world[0].length; i++) {
-			for (int j = 0; j < world[0][0].length; j++) {
-				world[0][i][j] = world[1][i][j];
-			}
-		}
 	}
 
 	private void resetCopyWorld() {
@@ -70,7 +100,6 @@ public class GameOfLife {
 		return counter;
 	}
 
-
 	private int countLivingCell(int x, int y) {
 		if (isInIndex(x,y) && world[0][x][y] == '*') {
 			return 1;
@@ -82,41 +111,11 @@ public class GameOfLife {
 		return (x > 0 && x < world[0].length) && (y > 0 && y < world[0].length);
 	}
 
-	private void printWorld(int worldIndex) {
+	private void copy() {
 		for (int i = 0; i < world[0].length; i++) {
 			for (int j = 0; j < world[0][0].length; j++) {
-				System.out.print(world[worldIndex][i][j]);
+				world[0][i][j] = world[1][i][j];
 			}
-			System.out.println();
-		}
-	}
-
-	private void createWorld() {
-		for (int i = 0; i < world[0].length; i++) {
-			for (int j = 0; j < world[0][0].length; j++) {
-				world[0][i][j] = '.';
-				world[1][i][j] = '.';
-			}
-		}
-	}
-
-	private void addRandomCells(int numberOfCellsAtStart) {
-		Random random = new Random();
-		int x, y;
-		for (int i = 0; i < numberOfCellsAtStart; i++) {
-			do {
-			y = random.nextInt(world[0].length);
-			x = random.nextInt(world[0].length);
-			} while (world[0][y][x] == '*');
-			world[0][y][x] = '*';
-		}
-	}
-	
-	private void clearScreen() {  
-	    try {
-			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}  
 }
--- Modul 411/GameOfLife/src/gameOfLife/Main.java
+++ Modul 411/GameOfLife/src/gameOfLife/Main.java
@@ -4,11 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			new GameOfLife(300);
+			new GameOfLife(60);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
 
 	}
 
